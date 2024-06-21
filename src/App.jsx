0454e3384e7311c870196fb847f5474380020072
Gumanpratapsingh <gumanpratapsinghparmar@gmail.com>
2024-06-21 17:59:20 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import { Signup } from "./pages/Signup";
 import { Signin } from "./pages/Signin";
 import { Task } from "./pages/Task";
@@ -8,6 +8,7 @@ function App() {
         <>
             <BrowserRouter>
                 <Routes>
+                    <Route path="/" element={<Navigate replace to="/signin" />} />
                     <Route path="/signup" element={<Signup />} />
                     <Route path="/signin" element={<Signin />} />
                     <Route path="/tasks" element={<Task />} />
